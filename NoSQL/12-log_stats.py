b2616b4220e77fae979a5d243fa2a7e8e8fdb8b1
Ornella Niyibizi <109486545+OrnellaNiyibizi@users.noreply.github.com>
2024-07-03 17:02:01 +0200

    Update 12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,35 +1,33 @@
 #!/usr/bin/env python3
-'''Task 12: Log stats
-'''
+"""
+Script that provides some stats about Nginx logs stored in MongoDB
+"""
 from pymongo import MongoClient
 
 
-def print_nginx_request_logs(nginx_collection):
-    '''Prints stats about Nginx request logs.
-    '''
-    try:
-        print('{} logs'.format(nginx_collection.count_documents({})))
-        print('Methods:')
-        methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-        for method in methods:
-            req_count = nginx_collection.count_documents({'method': method})
-            print('\tmethod {}: {}'.format(method, req_count))
-        status_checks_count = nginx_collection.count_documents({'method': 'GET', 'path': '/status'})
-        print('{} status check'.format(status_checks_count))
-    except Exception as e:
-        print("An error occurred:", e)
+METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
 
-def run():
-    '''Provides some stats about Nginx logs stored in MongoDB.
-    '''
-    try:
-        client = MongoClient('mongodb://127.0.0.1:27017')
-        nginx_collection = client.logs.nginx
-        print_nginx_request_logs(nginx_collection)
-    except Exception as e:
-        print("Failed to connect to MongoDB:", e)
+def log_stats(mongo_collection, option=None):
+    """
+    Provide some stats about Nginx logs stored in MongoDB
+    """
+    items = {}
+    if option:
+        value = mongo_collection.count_documents(
+            {"method": {"$regex": option}})
+        print(f"\tmethod {option}: {value}")
+        return
+
+    result = mongo_collection.count_documents(items)
+    print(f"{result} logs")
+    print("Methods:")
+    for method in METHODS:
+        log_stats(nginx_collection, method)
+    status_check = mongo_collection.count_documents({"path": "/status"})
+    print(f"{status_check} status check")
 
 
-if __name__ == '__main__':
-    run()
+if __name__ == "__main__":
+    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    log_stats(nginx_collection)
