75fbbbdeb9c826b6ebf876cf889e715e6ee312d1
OrnellaNiyibizi <o.mahoro@alustudent.com>
2024-07-19 21:47:48 +0200

    fix task 1 nodeJS basic

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,17 +1,13 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data}`);
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data}`);
-    process.exit();
-  });
+process.stdin.on('readable', () => {
+  const uname = process.stdin.read();
 
-  process.on('exit', () => {
-    console.log('This important software is now closing');
-  });
-}
+  if (uname) {
+    process.stdout.write(`Your name is: ${uname}`);
+  }
+});
+
+process.stdin.on('close', () => {
+  process.stdout.write('This important software is now closing\n');
+});
